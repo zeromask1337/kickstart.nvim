6c96553379e68c9bcd2c792494fc528eea826d33
D3monizer <vysmol@gmail.com>
2024-11-21 22:38:36 +0300

    refactor: configure indent plugin

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -4,6 +4,22 @@ return {
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
     main = 'ibl',
-    opts = {},
+    config = function()
+      local highlight = {
+        'CursorColumn',
+        'Whitespace',
+      }
+
+      require('ibl').setup {
+        indent = {
+          char = '',
+        },
+        whitespace = {
+          highlight = highlight,
+          remove_blankline_trail = false,
+        },
+        scope = { enabled = false },
+      }
+    end,
   },
 }
