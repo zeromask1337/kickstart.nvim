0f2afbbe00de9867830544a0bfcbd8c248cb181e
D3monizer <vysmol@gmail.com>
2024-12-15 18:36:38 +0300

    feat: add keymaps for quickfix list navigation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -25,4 +25,8 @@ map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Quickfix list navigation
+map('n', '<C-M-j>', '<cmd>cnext<CR>')
+map('n', '<C-M-k>', '<cmd>cprev<CR>')
+
 return {} -- Return empty table for consistency
