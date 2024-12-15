5999a657a1612691bf077e6ee77b63f32cfb3129
D3monizer <vysmol@gmail.com>
2024-12-15 14:10:04 +0300

    feat: set formatters for vue and ts files

--- init.lua
+++ init.lua
@@ -624,6 +624,8 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        vue = { 'eslint_d' },
+        typescript = { 'eslint_d', 'denols', stop_after_first = true },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
