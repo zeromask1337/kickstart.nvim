bdbf16633e76e8459b33a198ada9c1f067e6e2ef
D3monizer <vysmol@gmail.com>
2024-12-29 19:53:44 +0300

    refactor: extract gitsigns.nvim

--- lua/custom/plugins/buffer.lua
+++ lua/custom/plugins/buffer.lua
@@ -3,4 +3,30 @@ return {
   { 'tpope/vim-sleuth' },
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
+  -- Here is a more advanced example where we pass configuration
+  -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
+  --    require('gitsigns').setup({ ... })
+  --
+  -- See `:help gitsigns` to understand what the configuration keys do
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '┃' },
+        change = { text = '|' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked = { text = '┆' },
+      },
+      signs_staged = {
+        add = { text = '┃' },
+        change = { text = '┃' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked = { text = '┆' },
+      },
+    },
+  },
 }
