f07f0d8a0962a2cefb614af81220ccb93698b959
D3monizer <vysmol@gmail.com>
2025-06-27 13:57:33 +0300

    refactor: extract theme related plugins to separate file

--- init.lua
+++ init.lua
@@ -137,31 +137,6 @@ require('lazy').setup({
   --
   -- Use the `dependencies` key to specify the dependencies of a particular plugin
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-    end,
-  },
-  {
-    'kaiuri/nvim-juliana',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    lazy = false,
-    config = true,
-    init = function()
-      vim.cmd.colorscheme 'juliana'
-    end,
-  },
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
