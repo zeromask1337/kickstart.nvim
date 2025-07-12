9ca919c494b173c7f7c71e72c64410d8fab1c27b
D3monizer <vysmol@gmail.com>
2025-07-12 08:57:01 +0300

    refactor: revert lazydev filetypes to lua

--- lua/custom/plugins/lsp-config.lua
+++ lua/custom/plugins/lsp-config.lua
@@ -4,7 +4,7 @@ return {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
     -- used for completion, annotations and signatures of Neovim apis
     'folke/lazydev.nvim',
-    ft = 'lua,html,css,svelte',
+    ft = 'lua',
     opts = {
       library = {
         -- Load luvit types when the `vim.uv` word is found
