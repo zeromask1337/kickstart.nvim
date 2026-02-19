49986ea5cd26c69d77b6fcb5ef8efd61959b695d
D3monizer <vysmol@gmail.com>
2026-02-19 14:07:47 +0300

    refactor: change which-key preset to helix

--- lua/custom/plugins/which-key.lua
+++ lua/custom/plugins/which-key.lua
@@ -16,6 +16,7 @@ return {                -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     opts = {
+        preset = 'helix',
         icons = {
             -- set icon mappings to true if you have a Nerd Font
             mappings = vim.g.have_nerd_font,
