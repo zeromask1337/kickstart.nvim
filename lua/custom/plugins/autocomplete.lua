8056ffac6876a8b7b8c947795ff54887554c9c11
D3monizer <vysmol@gmail.com>
2026-02-19 12:08:45 +0300

    feat: add colorfull menu plugin

--- lua/custom/plugins/autocomplete.lua
+++ lua/custom/plugins/autocomplete.lua
@@ -1,65 +1,66 @@
 return {
-  { -- Autocompletion
-    'saghen/blink.cmp',
-    event = 'VimEnter',
-    version = '1.*',
-    dependencies = {
-      -- Snippet Engine
-      {
-        'L3MON4D3/LuaSnip',
-        version = '2.*',
-        build = (function()
-          -- Build Step is needed for regex support in snippets.
-          -- This step is not supported in many windows environments.
-          -- Remove the below condition to re-enable on windows.
-          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
+    "xzbdmw/colorful-menu.nvim",
+    { -- Autocompletion
+        'saghen/blink.cmp',
+        event = 'VimEnter',
+        version = '1.*',
         dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets.
-          --    See the README about individual language/framework/plugin snippets:
-          --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+            -- Snippet Engine
+            {
+                'L3MON4D3/LuaSnip',
+                version = '2.*',
+                build = (function()
+                    -- Build Step is needed for regex support in snippets.
+                    -- This step is not supported in many windows environments.
+                    -- Remove the below condition to re-enable on windows.
+                    if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+                        return
+                    end
+                    return 'make install_jsregexp'
+                end)(),
+                dependencies = {
+                    -- `friendly-snippets` contains a variety of premade snippets.
+                    --    See the README about individual language/framework/plugin snippets:
+                    --    https://github.com/rafamadriz/friendly-snippets
+                    -- {
+                    --   'rafamadriz/friendly-snippets',
+                    --   config = function()
+                    --     require('luasnip.loaders.from_vscode').lazy_load()
+                    --   end,
+                    -- },
+                },
+                opts = {},
+            },
         },
+        opts = {
+            keymap = {
+                preset = 'default',
+            },
+            appearance = {
+                nerd_font_variant = 'mono',
+            },
+            completion = {
+                ghost_text = { enabled = true },
+                documentation = { auto_show = false, auto_show_delay_ms = 500 },
+                menu = {
+                    draw = {
+                        columns = { { 'kind_icon', 'label', 'label_description', gap = 1 }, { 'kind' } },
+                    },
+                },
+            },
+            sources = {
+                default = { 'lsp', 'path', 'snippets', 'lazydev', 'buffer' },
+                providers = {
+                    lazydev = { name = 'LazyDev', module = 'lazydev.integrations.blink', score_offset = 100 },
+                },
+            },
+            snippets = { preset = 'default' },
+            fuzzy = { implementation = 'prefer_rust_with_warning' },
+            signature = { enabled = true },
+        },
+    },
+    {
+        'windwp/nvim-ts-autotag',
         opts = {},
-      },
     },
-    opts = {
-      keymap = {
-        preset = 'default',
-      },
-      appearance = {
-        nerd_font_variant = 'mono',
-      },
-      completion = {
-        ghost_text = { enabled = true },
-        documentation = { auto_show = false, auto_show_delay_ms = 500 },
-        menu = {
-          draw = {
-            columns = { { 'kind_icon', 'label', 'label_description', gap = 1 }, { 'kind' } },
-          },
-        },
-      },
-      sources = {
-        default = { 'lsp', 'path', 'snippets', 'lazydev', 'buffer' },
-        providers = {
-          lazydev = { name = 'LazyDev', module = 'lazydev.integrations.blink', score_offset = 100 },
-        },
-      },
-      snippets = { preset = 'default' },
-      fuzzy = { implementation = 'prefer_rust_with_warning' },
-      signature = { enabled = true },
-    },
-  },
-  {
-    'windwp/nvim-ts-autotag',
-    opts = {},
-  },
 }
